38c49325c0394bdd2c7bbd06b5fc7b3f3fccd8e9
Patrick <Patrick@172.29.78.187>
2017-02-02 21:04:56 -0500

    affichage du nouveau message

--- src/projetdudepot/ProjetDuDepot.java
+++ src/projetdudepot/ProjetDuDepot.java
@@ -15,7 +15,8 @@ public class ProjetDuDepot {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // Premiere affaire avec git
+        System.out.println("Hello world");
+        
     }
     
 }
